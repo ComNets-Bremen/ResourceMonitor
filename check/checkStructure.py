9afb94f9e658474a0c49b3e1ac2d9a35e651d0b0
Jens Dede <jd@comnets.uni-bremen.de>
2017-09-15 23:33:28 +0200

    Basic structure test script

---
+++ check/checkStructure.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""
+Perform a basic check if the ids are continuously increasing in the given json object
+"""
+
+import json
+import argparse
+import gzip
+
+argparser = argparse.ArgumentParser(description="Analze exported files from ResourceMonitor")
+argparser.add_argument('file', metavar='filename', type=str, help="Filename to process")
+argparser.add_argument('-z', "--gzip", action="store_true", help="Input file is gzip compressed")
+
+args = argparser.parse_args()
+
+opener = open
+if args.gzip:
+    print "Using gzip compressed json file"
+    opener = gzip.open
+
+with opener(args.file) as f:
+    data = json.load(f)
+
+    for item in data:
+        if hasattr(data[item], "__iter__"):
+            ids = []
+            for dataset in data[item]:
+                ids.append(dataset["_id"])
+            checkset = range(min(ids), max(ids)+1)
+            if len(ids) != len(checkset):
+                print "Data mismatch with \"" + item + "\":"
+                print "Missing:", list(set(checkset) - set(ids))
+
+print "Check done"
