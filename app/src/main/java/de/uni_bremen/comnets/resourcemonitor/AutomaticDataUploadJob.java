790d3d4e638dfa79f2ad747d7052165eed8af2ef
Jens Dede <jd@comnets.uni-bremen.de>
2017-11-09 13:33:51 +0100

    Ignore the release dir

---
+++ app/src/main/java/de/uni_bremen/comnets/resourcemonitor/AutomaticDataUploadJob.java
@@ -0,0 +1,10 @@
+package de.uni_bremen.comnets.resourcemonitor;
+
+import android.app.job.JobService;
+
+/**
+ * Created by jd on 06.11.17.
+ */
+
+public class AutomaticDataUploadJob extends JobService {
+}
