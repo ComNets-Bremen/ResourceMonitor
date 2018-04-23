6a47fbb4b601a10db1035361b0546d167a3afeda
Jens Dede <jd@comnets.uni-bremen.de>
2018-04-23 14:21:50 +0200

    Define basic event messages

---
+++ tools/OMNeT++/src/event_messages/EventMessages.h
@@ -0,0 +1,21 @@
+/*
+ * EventMessages.h
+ *
+ *  Created on: Apr 4, 2018
+ *      Author: jd
+ */
+
+#ifndef EVENTMESSAGES_H_
+#define EVENTMESSAGES_H_
+
+#include "ScreenEventMessage_m.h"
+#include "TrafficEventMessage_m.h"
+#include "BatteryEventMessage_m.h"
+#include "CellularEventMessage_m.h"
+#include "AirplaneModeEventMessage_m.h"
+#include "WiFiEventMessage_m.h"
+#include "BluetoothEventMessage_m.h"
+
+#include "EventTypeDefinitions.h"
+
+#endif /* EVENTMESSAGES_H_ */
