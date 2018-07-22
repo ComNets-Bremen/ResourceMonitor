02a011211fc42aedc0f50ca6f802fdb8ca1d6c0b
Jens Dede <jd@comnets.uni-bremen.de>
2018-07-22 23:26:04 +0200

    add logging

--- app/src/main/java/de/uni_bremen/comnets/resourcemonitor/BroadcastReceiver/DataUploadBroadcastReceiver.java
+++ app/src/main/java/de/uni_bremen/comnets/resourcemonitor/BroadcastReceiver/DataUploadBroadcastReceiver.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import de.uni_bremen.comnets.resourcemonitor.MonitorService;
 
@@ -12,6 +13,9 @@ import de.uni_bremen.comnets.resourcemonitor.MonitorService;
  * Triggered by the UploadWorker
  */
 public class DataUploadBroadcastReceiver extends AbstractResourceBroadcastReceiver {
+
+    private static final String TAG = DataUploadBroadcastReceiver.class.getSimpleName();
+
 
     // The action for the intent filter
     public static final String DATA_UPLOAD = DataUploadBroadcastReceiver.class.getCanonicalName() + ".ACTION";
@@ -22,6 +26,7 @@ public class DataUploadBroadcastReceiver extends AbstractResourceBroadcastReceiv
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "Received intent, starting upload job");
         // Trigger the upload using the MonitorService
         getMonitorServiceInstance().uploadToServer();
     }
