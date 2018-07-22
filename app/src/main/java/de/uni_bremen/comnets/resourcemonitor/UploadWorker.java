05e7835e18120dab8b468a21ed80d96a362b0ae6
Jens Dede <jd@comnets.uni-bremen.de>
2018-07-22 23:26:36 +0200

    rm context parameter, restructured constraints

--- app/src/main/java/de/uni_bremen/comnets/resourcemonitor/UploadWorker.java
+++ app/src/main/java/de/uni_bremen/comnets/resourcemonitor/UploadWorker.java
@@ -19,27 +19,34 @@ public class UploadWorker extends Worker {
 
     public static final String TAG = UploadWorker.class.getSimpleName();
 
-
-    private Context context = null;
-
-    public UploadWorker(Context context){
-        this.context = context;
-    }
+    static private boolean useUnmeteredConnection = true;
 
     @NonNull
     @Override
     public Result doWork() {
-        if (System.currentTimeMillis() > MonitorService.getLastServerUploadTimestamp(this.context) + MonitorService.MIN_AUTO_UPLOAD_INTERVAL * 1000) {
-        Intent intent = new Intent();
-        intent.setAction(DataUploadBroadcastReceiver.DATA_UPLOAD);
-        this.context.sendBroadcast(intent);
-        Log.d(TAG, "Sent upload intent");
+        Log.d(TAG, "doWork uploadWorker");
+        if (System.currentTimeMillis() > MonitorService.getLastServerUploadTimestamp(getApplicationContext()) + MonitorService.MIN_PERIOD_DATA_UPLOAD_INTERVAL * 1000) {
+
+            Intent intent = new Intent();
+            intent.setAction(DataUploadBroadcastReceiver.DATA_UPLOAD);
+            getApplicationContext().sendBroadcast(intent);
+            Log.d(TAG, "Sent upload intent");
+
     } else {
         Log.d(TAG, "Skipped current upload");
     }
         return Result.SUCCESS;
     }
 
+
+    /**
+     * True if the  upload is only performed using an unmetered connection
+     *
+     * @return true if unmetered, otherwise false
+     */
+    public static boolean onlyUnmeteredConnection(){
+        return useUnmeteredConnection;
+    }
 
     /**
      * Return the contraints for the worker
@@ -49,15 +56,20 @@ public class UploadWorker extends Worker {
      */
     public static Constraints getWorkerConstaints(Context context){
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        boolean useUnmeteredConnection = preferences.getBoolean("automatic_data_upload_only_on_unmetered_connection", true);
+
+        Constraints.Builder builder = new Constraints.Builder();
+        builder.setRequiresBatteryNotLow(true);
+
+        useUnmeteredConnection = preferences.getBoolean("automatic_data_upload_only_on_unmetered_connection", true);
+
         if (useUnmeteredConnection) {
-            return new Constraints.Builder().setRequiresBatteryNotLow(true).setRequiredNetworkType(NetworkType.UNMETERED).build();
-        }else {
-            return new Constraints.Builder().setRequiresBatteryNotLow(true).build();
+            builder.setRequiredNetworkType(NetworkType.UNMETERED);
         }
+
+        return builder.build();
     }
 
     public static PeriodicWorkRequest getWorkRequest(Context context){
-        return new PeriodicWorkRequest.Builder(UploadWorker.class, MonitorService.MAX_PERIOD_DATA_UPLOAD_INTERVAL, TimeUnit.SECONDS, MonitorService.MAX_PERIOD_DATA_UPLOAD_INTERVAL-MonitorService.MIN_DATA_UPLOAD_INTERVAL_LIMIT, TimeUnit.SECONDS).setConstraints(getWorkerConstaints(context)).build();
+        return new PeriodicWorkRequest.Builder(UploadWorker.class, MonitorService.MAX_PERIOD_DATA_UPLOAD_INTERVAL, TimeUnit.SECONDS, MonitorService.MAX_PERIOD_DATA_UPLOAD_INTERVAL-MonitorService.MIN_DATA_UPLOAD_INTERVAL_LIMIT, TimeUnit.SECONDS).setConstraints(getWorkerConstaints(context)).addTag(TAG).build();
     }
 }
