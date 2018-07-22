a469bd3f16ef27fbab6d6f52b853c9489afd75cd
Jens Dede <jd@comnets.uni-bremen.de>
2018-07-22 23:27:15 +0200

    logging

--- app/src/main/java/de/uni_bremen/comnets/resourcemonitor/MonitorServiceReceiver.java
+++ app/src/main/java/de/uni_bremen/comnets/resourcemonitor/MonitorServiceReceiver.java
@@ -3,16 +3,21 @@ package de.uni_bremen.comnets.resourcemonitor;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * Boot receiver to start the app on bootup
  */
 
 public class MonitorServiceReceiver extends BroadcastReceiver {
+    public static final String TAG = MonitorServiceReceiver.class.getSimpleName();
 
     @Override
     public void onReceive(Context context, Intent intent)
     {
+        Log.d(TAG, "Received intent: " +intent.getAction());
+        // TODO Check action before doing something
+
         Intent serviceIntent = new Intent(context, MonitorService.class);
         context.startService(serviceIntent);
     }
