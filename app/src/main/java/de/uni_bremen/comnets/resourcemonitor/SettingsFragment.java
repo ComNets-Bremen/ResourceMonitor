7075eb6893461f01e81b0ac5994bf649afaa8a04
Jens Dede <jd@comnets.uni-bremen.de>
2018-07-22 23:27:02 +0200

    Some comments regarding Android above API level 26+

--- app/src/main/java/de/uni_bremen/comnets/resourcemonitor/SettingsFragment.java
+++ app/src/main/java/de/uni_bremen/comnets/resourcemonitor/SettingsFragment.java
@@ -1,6 +1,8 @@
 package de.uni_bremen.comnets.resourcemonitor;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 
@@ -15,6 +17,18 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        // TODO: change for API level higher 26
+/*
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Disable the notification bar for Android versions with Doze
+            Preference notificationbar = this.findPreference("show_notification_bar");
+            notificationbar.getPreferenceManager().getSharedPreferences().edit().putBoolean("show_notification_bar", true).apply();
+            notificationbar.setShouldDisableView(true);
+            notificationbar.setEnabled(false);
+        }
+*/
+
     }
 
     @Override
